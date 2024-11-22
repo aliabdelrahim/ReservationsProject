9deb14f1848ff296a30fff8d02459a86128f506c
ali <ali.abdelrahim@hotmail.be>
2024-11-22 22:53:44 +0100

    Controller Artist +templates +routes

---
+++ src/main/java/com/reservations/reservations/controller/ArtistController.java
@@ -0,0 +1,40 @@
+package com.reservations.reservations.controller;
+
+import java.util.List;
+
+import com.reservations.reservations.model.Artist;
+import com.reservations.reservations.service.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@Controller
+public class ArtistController {
+    @Autowired
+    ArtistService service;
+
+    @GetMapping("/artists")
+    public String index(Model model) {
+        List<Artist> artists = service.getAllArtists();
+
+        model.addAttribute("artists", artists);
+        model.addAttribute("title", "Liste des artistes");
+
+        return "artist/index";
+    }
+    @GetMapping("/artists/{id}")
+    public String show(Model model, @PathVariable("id") long id) {
+        Artist artist = service.getArtist(id);
+
+        model.addAttribute("artist", artist);
+        model.addAttribute("title", "Fiche d'un artiste");
+
+        return "artist/show";
+    }
+
+
+}
+
--- src/main/java/com/reservations/reservations/controller/HomeController.java
+++ src/main/java/com/reservations/reservations/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.reservations.reservations;
+package com.reservations.reservations.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home"; // Cette vue doit correspondre à un fichier home.html dans le dossier templates
+        return "layouts/home"; // Cette vue doit correspondre à un fichier home.html dans le dossier templates
     }
 }
