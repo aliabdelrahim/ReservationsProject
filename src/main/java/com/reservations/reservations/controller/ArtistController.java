6f4d436a7aa6d9c08ed05244c945b60bbc7bd20c
ali <ali.abdelrahim@hotmail.be>
2024-11-24 20:54:29 +0100

    Form create Artist

--- src/main/java/com/reservations/reservations/controller/ArtistController.java
+++ src/main/java/com/reservations/reservations/controller/ArtistController.java
@@ -9,11 +9,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 @Controller
@@ -77,6 +74,28 @@ public class ArtistController {
 
         return "redirect:/artists/"+artist.getId();
     }
+
+    @GetMapping("/artists/create")
+    public String create(Model model) {
+        Artist artist = new Artist(null,null);
+
+        model.addAttribute("artist", artist);
+
+        return "artist/create";
+    }
+
+    @PostMapping("/artists/create")
+    public String store(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "artist/create";
+        }
+
+        service.addArtist(artist);
+
+        return "redirect:/artists/"+artist.getId();
+    }
+
 
 }
 
--- src/main/java/com/reservations/reservations/model/Artist.java
+++ src/main/java/com/reservations/reservations/model/Artist.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -15,12 +16,13 @@ public class Artist {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "The firstname must not be empty.")
+    @NotEmpty(message = "The firstname must not be empty.")
     @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
+
 
     private String firstname;
 
-    @NotBlank(message = "The lastname must not be empty.")
+    @NotEmpty(message = "The lastname must not be empty.")
     @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
 
     private String lastname;
