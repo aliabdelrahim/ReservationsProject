02083428685386dd97ec734f0ebb10d01e26f415
ali <ali.abdelrahim@hotmail.be>
2025-02-12 19:47:51 +0100

    Update templates Show (added artists’ list)

--- src/main/java/com/reservations/reservations/controller/ShowController.java
+++ src/main/java/com/reservations/reservations/controller/ShowController.java
@@ -1,6 +1,8 @@
 package com.reservations.reservations.controller;
 
 import com.reservations.reservations.model.Show;
+import com.reservations.reservations.model.Artist;
+import com.reservations.reservations.model.ArtistType;
 import com.reservations.reservations.service.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,9 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 @Controller
 public class ShowController {
@@ -35,7 +40,20 @@ public class ShowController {
         // On récupère l'objet Show, et on vérifie si il existe
         Show show = (Show) service.getShowById(Long.valueOf(id))
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Spectacle non trouvé"));
+        //Récupérer les artistes du spectacle et les grouper par type
+        Map<String,ArrayList<Artist>> collaborateurs = new TreeMap<>();
 
+        for(ArtistType at : show.getArtistTypes()) {
+            String type = at.getType().getType();
+
+            if(collaborateurs.get(type) == null) {
+                collaborateurs.put(type, new ArrayList<>());
+            }
+
+            collaborateurs.get(type).add(at.getArtist());
+        }
+
+        model.addAttribute("collaborateurs", collaborateurs);
         model.addAttribute("show", show);
         model.addAttribute("title", "Fiche d'un spectacle");
 
